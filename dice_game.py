c25c9b1010b84de7b2e353114d54c7850e563441
Nakada Juri <157940688+nak-juuri7@users.noreply.github.com>
2025-07-03 22:14:12 +0900

    Dice game: initial revision

---
+++ dice_game.py
@@ -0,0 +1,10 @@
+import random
+
+#Dice Game
+roll_dice1 = random.randint(1,6)
+roll_dice2 = random.randint(1,6)
+total = roll_dice1 + roll_dice2
+print("Rolling dice...")
+print(f"Die 1: {(roll_dice1)}")
+print(f"Die 2: {(roll_dice2)}")
+print(f"Total value: {(total)}")
